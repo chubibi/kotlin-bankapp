c26bb324d4b8eacd91ac9d9e79637dda80329b4d
ChubisJos <josue.cazares.10@gmail.com>
2024-01-02 16:11:03 -0600

    Add drawable style button

--- app/src/main/java/com/example/horoscapp/ui/luck/LuckFrg.kt
+++ app/src/main/java/com/example/horoscapp/ui/luck/LuckFrg.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.horoscapp.databinding.FrgLuckBinding
+import com.google.android.material.badge.BadgeDrawable
 
 class LuckFrg : Fragment() {
     private var _binding: FrgLuckBinding? = null
@@ -16,5 +17,9 @@ class LuckFrg : Fragment() {
     ): View {
         _binding = FrgLuckBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
